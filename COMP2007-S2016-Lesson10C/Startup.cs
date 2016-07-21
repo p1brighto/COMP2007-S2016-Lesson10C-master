03fd169681413df936f8ef701dde49deb487c135
p1brighto <p1brighto@gmail.com>
2016-07-21 17:47:03 -0400

    Add project files.

---
+++ COMP2007-S2016-Lesson10C/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_S2016_Lesson10C.Startup))]
+namespace COMP2007_S2016_Lesson10C
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
